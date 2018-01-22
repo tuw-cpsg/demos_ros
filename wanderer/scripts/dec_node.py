0e2104ada1580c79cd2314d88bf751134de8ad79
Tomas Thalmann <tthalmann@gmail.com>
2018-01-22 22:56:29 +0100

    added point cloud utilities

--- wanderer/scripts/dec_node.py
+++ wanderer/scripts/dec_node.py
@@ -8,6 +8,7 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from sensor_msgs.msg import LaserScan
 
+
 def quat2euler(q):
     a, b, c, d = q[0], q[1], q[2], q[3]
 
@@ -22,6 +23,7 @@ def quat2euler(q):
     y = m.atan2(c21, c11)
 
     return r, p, y
+
 
 class DecisionMaker():
 
@@ -65,9 +67,9 @@ class DecisionMaker():
         _, _, self.theta = quat2euler(quat)
 
         pos_pred = None
-        d = self.lin_vel * self.dt # CHANGE self.dt
+        d = self.lin_vel * self.dt  # CHANGE self.dt
         for w in self.range_w:
-            tp = self.theta + w * self.dt # CHANGE self.dt
+            tp = self.theta + w * self.dt  # CHANGE self.dt
             xp = pos[0] + d * m.cos(tp)
             yp = pos[1] + d * m.sin(tp)
             if pos_pred is None:
@@ -83,41 +85,38 @@ class DecisionMaker():
         twist.linear.x = self.lin_vel
         twist.angular.z = self.ang_vel
         self.pub.publish(twist)
-  
+
     def callbackLaser(self, data):
         ranges, angles = self._convert_data(data)
-        in_front = np.argmin(np.abs(angles))-5, np.argmin(np.abs(angles)) + 5
+        in_front = np.argmin(np.abs(angles)) - 5, np.argmin(np.abs(angles)) + 5
         # print(ranges[in_front[0]:in_front[1]])
         max_pos = np.argmax(ranges)
         delta_theta = angles[max_pos]
         print(delta_theta)
-        self.theta_ls = self.theta + delta_theta 
+        self.theta_ls = self.theta + delta_theta
 
     def _convert_data(self, data):
-        ranges = np.array(data.ranges) 
+        ranges = np.array(data.ranges)
         angles = np.arange(len(data.ranges))
         angles = data.angle_min + angles * data.angle_increment
         return ranges, angles
 
 
 def main():
-
     pub = rospy.Publisher('/teleop/cmd_vel', Twist, queue_size=10)
     dec = DecisionMaker(pub)
     twist = Twist()
-    #lin_vel = 0.1
+    # lin_vel = 0.1
     lin_vel = 0
 
     rospy.Subscriber('/cps_pe/kfestimate', PoseWithCovarianceStamped,
                      dec.callbackKF)
     rospy.Subscriber('/scan', LaserScan, dec.callbackLaser)
 
-    rospy.spin() 
+    rospy.spin()
 
-        
-        
 
 if __name__ == '__main__':
     rospy.init_node('wanderer_decision', anonymous=True)
-    
+
     main()
