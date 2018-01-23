d10fe9b6b1c3fef473cd3743da27cd17e45ddeb7
Tomas Thalmann <tthalmann@gmail.com>
2018-01-23 07:54:34 +0100

    some changes in VM

--- wanderer/scripts/dec_node.py
+++ wanderer/scripts/dec_node.py
@@ -7,6 +7,12 @@ import time
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+
+MAX_DIST_THRESHOLD = 1.
+DT = 0.1
+SPECIAL_MOVEMENT = False
+
 
 def quat2euler(q):
     a, b, c, d = q[0], q[1], q[2], q[3]
@@ -30,7 +36,12 @@ class DecisionMaker():
         self.lin_vel = 0.1
         self.theta = 0
         self.theta_ls = None
+        self.max_dist = None
         self.pub = pub
+        
+        self.MAX_DIST_THRESHOLD = 1.
+        self.DT = 0.3
+        self.SPECIAL_MOVEMENT = False
 
     def callbackKF(self, data):
         quat = np.array([data.pose.pose.orientation.w,
@@ -39,30 +50,46 @@ class DecisionMaker():
                          data.pose.pose.orientation.z])
         _, _, self.theta = quat2euler(quat)
 
+        if self.theta_ls is None or self.max_dist is None or self.SPECIAL_MOVEMENT:
+            return
+
+        if self.max_dist < self.MAX_DIST_THRESHOLD:
+            twist = Twist()
+            twist.linear.x = 0
+            twist.angular.z = 0.3
+            self.pub.publish(twist)
+            self.SPECIAL_MOVEMENT = True
+            time.sleep(np.pi/0.3)
+            self.SPECIAL_MOVEMENT = False
+
         range_w = np.arange(-0.3, 0.35, 0.05)
         utility_dtheta = np.array([[0, 0]])
         for w in range_w:
-            theta_pi = self.theta + 0.3 * (self.ang_vel + w)
+            # theta_pi = self.theta + 0.3 * (self.ang_vel + w)
+            theta_pi = self.theta + self.DT * w
             dtheta = theta_pi - self.theta_ls
-            pi2 = m.pi / 2
+            # print('Theta, dtheta: {} | {}'.format(self.theta, dtheta))
+            pi2 = m.pi
             utility = (pi2 - m.fabs(dtheta)) / pi2
             utility_dtheta = np.vstack((utility_dtheta,
-                                        np.array([[utility, dtheta]])))
+                                        np.array([[utility, w]])))
+        #print(utility_dtheta)
+        # self.ang_vel = range_w[np.argmax(utility)]
+        # self.ang_vel = utility_dtheta[np.argmax(utility_dtheta[:, 0]), 1]
+        # print('{}|{}'.format(self.theta_ls-self.theta, self.ang_vel))
+        # twist = Twist()
+        # twist.linear.x = self.lin_vel
+        # twist.angular.z = self.ang_vel
+        # self.pub.publish(twist)
 
-        self.ang_vel = utility_dtheta[np.argmax[utility_dtheta[:, 0]], 1] / 0.3
-        twist = Twist()
-        twist.linear.x = self.lin_vel
-        twist.angular.z = self.ang_vel
-        self.pub.publish(twist)
-  
     def callbackLaser(self, data):
         ranges, angles = self._convert_data(data)
         in_front = np.argmin(np.abs(angles))-5, np.argmin(np.abs(angles)) + 5
-        # print(ranges[in_front[0]:in_front[1]])
         max_pos = np.argmax(ranges)
         delta_theta = angles[max_pos]
-        print(delta_theta)
-        self.theta_ls = self.theta + delta_theta 
+        print('DTheta: {}'.format(delta_theta/np.pi*180))
+        self.theta_ls = self.theta + delta_theta
+        self.max_dist = np.nanmax(ranges) 
 
     def _convert_data(self, data):
         ranges = np.array(data.ranges) 
@@ -79,7 +106,9 @@ def main():
     #lin_vel = 0.1
     lin_vel = 0
 
-    rospy.Subscriber('/cps_pe/kfestimate', PoseWithCovarianceStamped,
+    # rospy.Subscriber('/cps_pe/kfestimate', PoseWithCovarianceStamped,
+    #                  dec.callbackKF)
+    rospy.Subscriber('/p2os/pose', Odometry,
                      dec.callbackKF)
     rospy.Subscriber('/scan', LaserScan, dec.callbackLaser)
 
